b5a247ecd0415b26ddab4c9581f8c7427994f34a
Yaki <yaki.koren@gmail.com>
2018-09-11 16:58:54 +0300

    call with parameters

--- JavaScript/src/spies.js
+++ JavaScript/src/spies.js
@@ -1,11 +1,30 @@
 var ToLiteral = require('./toLiteral')
 
-module.exports.getCallParamsDeclarationStringSpy =
-	function (callingFunctionArguments, varPrefix) {
-		var declarations = ''
-		Array.from(callingFunctionArguments).forEach((argument,index) => {
-			declarations+='var '+varPrefix+'_param'+index+' = '+ToLiteral.toLiteral(argument)+'\n'
-		})
-		return declarations;
+module.exports.getDefinitionAndCallingStringSpy = 
+function (callingFunctionArguments, functionName, paramString) {
+	var theString = '/****** Prep/Call Function ' + functionName +' ********/\n'
+	Array.from(callingFunctionArguments).forEach((argument,index) => {
+		theString+='var '+getParamName(functionName,paramString,index)+' = '+ToLiteral.toLiteral(argument)+'\n'
+	})
+	theString += '\n'
+	theString += functionName + '('
+	Array.from(callingFunctionArguments).forEach((argument, index) => {
+		if (index>0) theString+=', '
+		theString+=getParamName(functionName,paramString,index)
+	})
+	theString += ')\n\n'
+
+	return theString;
+
+	}
+
+var getParamName = function (functionName, paramString, paramIndex) {
 	
+	if (paramString === undefined)
+		return functionName + '_param' + paramIndex
+	else {
+		var paramArray = paramString.split(',')
+		return paramArray[paramIndex].trim()
+	}
+		
 }
--- JavaScript/test/spies-getFunctionCallStringTests.js
+++ JavaScript/test/spies-getFunctionCallStringTests.js
@@ -7,18 +7,49 @@ var Spies = require('../src/spies')
 
 mocha.describe('get call string', function () {
 
-    mocha.it('basic test', function () {
+    mocha.it('should return definitions/calling statements (no param names)', function () {
 
         var a = [1, 2, 3]
         var b = { q: 1, w: a }
         var callString = ''    
         var testFunction = function () {
-            callString = Spies.getCallParamsDeclarationStringSpy(arguments,'testFunction')
+            callString = Spies.getDefinitionAndCallingStringSpy(arguments, 'testFunction')
+            return a[0]+b.q
         }
         testFunction(a, b, 2)
         
 
         expect(callString)
-            .equals('var testFunction_param0 = [1,2,3]\nvar testFunction_param1 = {q:1,w:[1,2,3]}\nvar testFunction_param2 = 2\n')
+            .equals('/****** Prep/Call Function testFunction ********/\n'+
+                    'var testFunction_param0 = [1,2,3]\n' +
+                    'var testFunction_param1 = {q:1,w:[1,2,3]}\n' +
+                    'var testFunction_param2 = 2\n' +
+                    '\n' +
+                    'testFunction(testFunction_param0, testFunction_param1, testFunction_param2)\n' +
+            '\n')
+        expect (eval(callString)).equals(2)
+    })
+
+    mocha.it('should return definitions/calling statements (with param names)', function () {
+
+        var a = [1, 2, 3]
+        var b = { q: 1, w: a }
+        var callString = ''    
+        var testFunction = function (A,B,C) {
+            callString = Spies.getDefinitionAndCallingStringSpy(arguments, 'testFunction','A,B ,C')
+            return a[0]+b.q
+        }
+        testFunction(a, b, 2)
+        
+
+        expect(callString)
+            .equals('/****** Prep/Call Function testFunction ********/\n'+
+                    'var A = [1,2,3]\n' +
+                    'var B = {q:1,w:[1,2,3]}\n' +
+                    'var C = 2\n' +
+                    '\n' +
+                    'testFunction(A, B, C)\n' +
+            '\n')
+        expect (eval(callString)).equals(2)
     })
 })
