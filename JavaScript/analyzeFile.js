29d45201f1147224741feef94b92f4fa1e3552b8
Yaki <yaki.koren@gmail.com>
2018-09-12 12:32:38 +0300

    removed unnecessary files

--- JavaScript/analyzeFile.js
+++
@@ -1,5 +0,0 @@
-var analyzeCode = require('./src/analyzeCode.js')
-
-var fs = require("fs")
-var text = fs.readFileSync(process.argv[2]).toString('utf-8')
-console.log(analyzeCode.getAnalyzedFunctions(text))
