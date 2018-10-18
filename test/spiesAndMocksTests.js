ae79895a900626d6a765e0e61a6e627c37cee7a4
Yaki <yaki.koren@gmail.com>
2018-10-18 22:27:58 +0300

    another test

--- test/spiesAndMocksTests.js
+++ test/spiesAndMocksTests.js
@@ -12,13 +12,13 @@ var expect = chai.expect
 mocha.describe('Spies and Mocks', function () {
 
     mocha.it('long Function name', function () {
-        var b = { q: 1, w: {a:1,b:function(){return 3}} }
+        var b = { q: 1, w: { a: 1, b: function () { return 3 } } }
         var harness = ''
         var testFunction = function () {
             var mySpy = new RuntimeSpy('mySpy')
             mySpy.setTestFunctionCall("testFunction()")
             var text = mySpy.addVariablesSpies({ 'b.w.b': b.w.b }).getCodeToEvalToSpyOnVariables()
-            
+
             eval(text)
             var a = b.w.b()
             harness = mySpy.getHarness()
@@ -33,7 +33,7 @@ mocha.describe('Spies and Mocks', function () {
         var b = { q: 1, w: a }
         var harness = ''
         var testFunction = function () {
-           var mySpy = new RuntimeSpy('mySpy')
+            var mySpy = new RuntimeSpy('mySpy')
             mySpy.setTestFunctionCall("testFunction()")
             eval(mySpy.getCodeToEvalToSpyOnVariables())
             harness = mySpy.getHarness()
@@ -53,8 +53,8 @@ mocha.describe('Spies and Mocks', function () {
         var harness = ''
         var testFunction = function (A, B, C) {
             var mySpy = new RuntimeSpy('mySpy')
-            mySpy.setStartFunctionCall(arguments, 'testFunction', 'A,B,C')
-            eval(mySpy.getCodeToEvalToSpyOnVariables())
+            mySpy.setTestFunctionCall("testFunction(a,b,2)")
+            eval(mySpy.addVariablesSpies({ a: a, b: b }).getCodeToEvalToSpyOnVariables())
             harness = mySpy.getHarness()
             return a[0] + b.q
         }
@@ -84,12 +84,12 @@ mocha.describe('Spies and Mocks', function () {
         var testFunction = function (A) {
 
             var mySpy = new RuntimeSpy('mySpy')
-            mySpy.setStartFunctionCall(arguments, 'testFunction','A')
+            mySpy.setStartFunctionCall(arguments, 'testFunction', 'A')
             var myText = mySpy.addVariablesSpies({ globalVar: globalVar, globalVar2: globalVar2, helper1: helper1, helper2: helper2 }).getCodeToEvalToSpyOnVariables()
             eval(myText)
             helper1(21)
             var a = globalVar2['4']['12'](3)
-            var result = a+helper1(A) + helper2(A) + globalVar + globalVar2['3']['2']['1'] + globalVar2['4']['12'](4)
+            var result = a + helper1(A) + helper2(A) + globalVar + globalVar2['3']['2']['1'] + globalVar2['4']['12'](4)
             mySpy.addFinalResult(result)
             harness = mySpy.getHarness()
 
@@ -107,8 +107,8 @@ mocha.describe('Spies and Mocks', function () {
     })
 
     mocha.it('Mocks2', function () {
-//empty, for temporary tests
-       
+        //empty, for temporary tests
+
     })
 
 })
