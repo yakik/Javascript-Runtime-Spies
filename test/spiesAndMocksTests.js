f00a98e197182839c2905d6b0879ac8c8d46b5d9
Yaki <yaki.koren@gmail.com>
2018-10-19 13:19:33 +0300

    editing, simplfying the tests

--- test/spiesAndMocksTests.js
+++ test/spiesAndMocksTests.js
@@ -4,54 +4,58 @@ var mocha = require('mocha')
 var chai = require('chai')
 var expect = chai.expect
 
-
-
-
 mocha.describe('Spies and Mocks', function () {
 
     mocha.it('long Function name', function () {
+        var expectedJSON = {
+            testedFuctionCall: 'testFunction()',
+            resultLiteral: undefined,
+            variables: [{ name: 'b.w.b', values: '{input:[[]],output:[3]}' }]
+        }
         var b = { q: 1, w: { a: 1, b: function () { return 3 } } }
         var testFunction = function () {
             var mySpy = new RuntimeSpy('mySpy')
             mySpy.setTestFunctionCall("testFunction()")
             eval(mySpy.addVariablesSpies({ 'b.w.b': b.w.b }).getCodeToEvalToSpyOnVariables())
             var a = b.w.b()
-            var expectedJSON = {
-                testedFuctionCall: 'testFunction()',
-                resultLiteral: undefined,
-                variables: [{ name: 'b.w.b', values: '{input:[[]],output:[3]}' }]
-            }
+
             expect(expectedJSON).to.deep.equal(mySpy.getHarnessNew())
-            return a
         }
         testFunction()
     })
 
     mocha.it('should return definitions/calling statements (no param names)', function () {
+        var expectedJSON = {
+            testedFuctionCall: 'testFunction()',
+            resultLiteral: undefined,
+            variables: []
+        }
         var a = [1, 2, 3]
         var b = { q: 1, w: a }
         var testFunction = function () {
             var mySpy = new RuntimeSpy('mySpy')
             mySpy.setTestFunctionCall("testFunction()")
             eval(mySpy.getCodeToEvalToSpyOnVariables())
-            harness = mySpy.getHarness()
-
-            var expectedJSON = {
-                testedFuctionCall: 'testFunction()',
-                resultLiteral: undefined,
-                variables: []
-            }
             expect(expectedJSON).to.deep.equal(mySpy.getHarnessNew())
-            return a[0] + b.q
         }
         testFunction()
     })
 
 
-
-
-
     mocha.it('should return definitions/calling statements (with param names)', function () {
+        var expectedJSON = {
+            testedFuctionCall: 'testFunction(a,b,2)',
+            resultLiteral: undefined,
+            variables:
+                [{
+                    name: 'a',
+                    values: 'new Map([[\'Initial\',\'a = [1,2,3]\']])'
+                },
+                {
+                    name: 'b',
+                    values: 'new Map([[\'Initial\',\'b = {q:1,w:[1,2,3]}\']])'
+                }]
+        }
         var a = [1, 2, 3]
         var b = { q: 1, w: a }
         var testFunction = function (A, B, C) {
@@ -59,29 +63,35 @@ mocha.describe('Spies and Mocks', function () {
             mySpy.setTestFunctionCall("testFunction(a,b,2)")
             eval(mySpy.addVariablesSpies({ a: a, b: b }).getCodeToEvalToSpyOnVariables())
 
-            var expectedJSON = {
-                testedFuctionCall: 'testFunction(a,b,2)',
-                resultLiteral: undefined,
-                variables:
-                    [{
-                        name: 'a',
-                        values: 'new Map([[\'Initial\',\'a = [1,2,3]\']])'
-                    },
-                    {
-                        name: 'b',
-                        values: 'new Map([[\'Initial\',\'b = {q:1,w:[1,2,3]}\']])'
-                    }]
-            }
+
             expect(expectedJSON).to.deep.equal(mySpy.getHarnessNew())
             return a[0] + b.q
         }
         testFunction(a, b, 2)
     })
 
-
-
     mocha.it('Mocks', function () {
 
+        var expectedJSON = {
+            testedFuctionCall: 'testFunction(A)',
+            resultLiteral: 'result = 76',
+            variables:
+                [{ name: 'A', values: 'new Map([[\'Initial\',\'A = 5\']])' },
+                {
+                    name: 'globalVar',
+                    values: 'new Map([[\'Initial\',\'globalVar = 5\'],[\'helper1_0\',\'globalVar = 42\'],[\'helper1_1\',\'globalVar = 10\']])'
+                },
+                {
+                    name: 'globalVar2',
+                    values: 'new Map([[\'Initial\',\'globalVar2 = {1:6,2:2,3:{1:1},4:{1:4,12:function(){}}};globalVar2[\\\'3\\\'][\\\'2\\\']=globalVar2\']])'
+                },
+                {
+                    name: 'globalVar2[\'4\'][\'12\']',
+                    values: '{input:[[3],[4]],output:[15,20]}'
+                },
+                { name: 'helper1', values: '{input:[[21],[5]],output:[42,10]}' },
+                { name: 'helper2', values: '{input:[[5]],output:[15]}' }]
+        }
         var helper1 = function (x) {
 
             globalVar = 2 * x
@@ -104,36 +114,11 @@ mocha.describe('Spies and Mocks', function () {
             var a = globalVar2['4']['12'](3)
             var result = a + helper1(A) + helper2(A) + globalVar + globalVar2['3']['2']['1'] + globalVar2['4']['12'](4)
             mySpy.addFinalResult(result)
-            var expectedJSON = {
-                testedFuctionCall: 'testFunction(A)',
-                resultLiteral: 'result = 76',
-                variables:
-                    [{ name: 'A', values: 'new Map([[\'Initial\',\'A = 5\']])' },
-                    {
-                        name: 'globalVar',
-                        values: 'new Map([[\'Initial\',\'globalVar = 5\'],[\'helper1_0\',\'globalVar = 42\'],[\'helper1_1\',\'globalVar = 10\']])'
-                    },
-                    {
-                        name: 'globalVar2',
-                        values: 'new Map([[\'Initial\',\'globalVar2 = {1:6,2:2,3:{1:1},4:{1:4,12:function(){}}};globalVar2[\\\'3\\\'][\\\'2\\\']=globalVar2\']])'
-                    },
-                    {
-                        name: 'globalVar2[\'4\'][\'12\']',
-                        values: '{input:[[3],[4]],output:[15,20]}'
-                    },
-                    { name: 'helper1', values: '{input:[[21],[5]],output:[42,10]}' },
-                    { name: 'helper2', values: '{input:[[5]],output:[15]}' }]
-            }
+           
             expect(expectedJSON).to.deep.equal(mySpy.getHarnessNew())
-            return result
         }
 
-        expect(testFunction(5)).equals(76)
-    })
-
-    mocha.it('Mocks2', function () {
-        //empty, for temporary tests
-
+        testFunction(5)
     })
 
 })
